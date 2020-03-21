dde24a0cf8e14e756a2f3af168529fb2a6112574
GamezArt94 <46079765+GamezAr94@users.noreply.github.com>
2020-03-21 11:10:12 -0700

    Creating Script to the tiles and buttons
    Adding actions to the buttons to interact with the grid adding water, start point end point tiles.

---
+++ AstarPathFinding2D/Assets/Scripts/AStar.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public enum TileType { STAR, GOAL, WATER, GRASS, PATH}
+
+public class AStar : MonoBehaviour
+{
+    [SerializeField]
+    private Camera camera;
+
+    [SerializeField]
+    private Tile[] tiles;
+
+    [SerializeField]
+    private Tilemap tileMap;
+
+    [SerializeField]
+    private LayerMask layerMask;
+
+    private TileType tileType;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(camera.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, Mathf.Infinity, layerMask);
+            if(hit.collider != null)
+            {
+                Vector3 mouseWorlPos = camera.ScreenToWorldPoint(Input.mousePosition);
+                Vector3Int clickPos = tileMap.WorldToCell(mouseWorlPos);
+                ChangeTile(clickPos);
+            }
+        }
+    }
+
+    public void ChangeTileType(TileButton button)
+    {
+        tileType = button.MyTileType;
+    }
+
+    private void ChangeTile(Vector3Int clickPos)
+    {
+        tileMap.SetTile(clickPos, tiles[(int)tileType]);
+    }
+}
